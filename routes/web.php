38a919c82e6f5a50a2fe9adf393000f059cd3a87
Ivan <151478162+ranadaivan@users.noreply.github.com>
2024-05-03 18:48:46 +0800

    setting reg as homepage

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('register');
 });
 
 Route::get('/posts/create', function() {
